d2ddf43a5b89cd43f2b5bf9ad51da7bf152da208
ThaiBao107 <nguyenbao1072004@gmail.com>
2023-06-09 15:36:55 +0700

    update_09/06/2023

--- BTH1.cpp
+++ BTH1.cpp
@@ -60,7 +60,7 @@ void Them_vao_dau(LIST &l, NODE *p)
     else
     {
         p->link = l.pHead;
-        l.pHead =p;
+        l.pHead = p;
     }
 }
 
@@ -80,25 +80,64 @@ void Them_Vao_Cuoi(LIST &l, NODE *p)
 
 void xuatDS(LIST l)
 {
+    cout<<"| "<<setw(9)<<left<<"Ma code"<<" |"<<setw(17)<<left<<"NAME"<<" |"<<setw(4)<<left<<"TUOI"<<" |"<<endl;
     for(NODE *k = l.pHead; k!=NULL;k=k->link)
     {
         cout<<"| "<<setw(9)<<left<< k->thong_tin.code;
         cout<<" |"<<setw(17)<<left<< k->thong_tin.name;
         cout<<" |"<<setw(4)<<left<< k->thong_tin.tuoi<<" |";
+        cout<<endl;
     }
 }
 
+
+// them p vao sau q   // xem lai
+// ---------------------------------------------------------
+void them_p_vaosau_q(LIST &l,NODE *p)
+{
+    I t;
+    int x;
+    cout<<"Nhap node q can them: ";
+    cin >> x;
+    NODE* q = KhoitaoNode(t,x);
+    int count =0;
+    // chạy for trước để tìm nốt q cần thêm vào 
+    if( q->n == l.pHead->n && l.pHead == NULL)
+    {
+        Them_Vao_Cuoi(l,p);
+    }
+    else
+    {
+    for(NODE *k = l.pHead;k!=NULL;k=k->link)
+    {
+        if(q ->n == k->n)
+        {
+            NODE *h = KhoitaoNode(k->thong_tin,k->n);
+            NODE *g = k->link;
+            h->link = g;
+            k->link = h;
+            count++;
+        }
+    }
+    }
+    if(count != 0) cout<<"Them vao thanh cong"<<endl;
+    else
+        cout<<"Khong tim thay node q"<<endl;
+}
+
+// ---------------------------------------------------------
 int main()
 {
     LIST l;
     D(l);
     int n;
+    do
     {
         cout<<"1. Tao 1 node"<<endl;
         cout<<"2. Xuat Danh sach"<<endl;
-        cout<<"2. Them vao dau"<<endl;
-        cout<<"4. Them vao cuoi"<<endl;
-        cout<<"3. EXIT"<<endl;
+        cout<<"3. Them vao dau"<<endl;
+        cout<<"4. Them vao p vao sau node q"<<endl;
+        cout<<"5. EXIT"<<endl;
         cout<<"Choose: "; 
         while(true)
         {
@@ -108,7 +147,6 @@ int main()
                 cout<<"Nhap sai: ";
         }
         system("cls");
-        
         switch(n)
         {
             case 1:
@@ -128,12 +166,29 @@ int main()
                 {
                 cout<<"THONG TIN SINH VIEN"<<endl;
                 // Ma code         NAME         Tuoi
-                cout<<"| "<<setw(9)<<left<<"Ma code"<<" |"<<setw(17)<<left<<"NAME"<<" |"<<setw(4)<<left<<"TUOI"<<" |"<<endl;
                 xuatDS(l);
-                cout<<endl;
                 break;
                 } 
-            case 3:
+
+            case 4:
+            {
+                I thong_tin;
+                int n;
+                cout<<"Nhap node p: "; cin >> n;
+                cin.ignore();
+                cout<<"Nhap MSSV: "; getline(cin,thong_tin.code);
+                cout<<"Nhap name: "; getline(cin,thong_tin.name);
+                cout<<"Nhap tuoi: "; cin >> thong_tin.tuoi;
+                NODE *p = KhoitaoNode(thong_tin,n);
+                system("cls");
+                // ---------------------------------------------
+                them_p_vaosau_q(l,p);
+                cout<<"-> Nhan enter de xem KQ: ";
+                getch();
+                system("cls");
+                D(l);
+            }
+            case 5:
                 {
                 cout<<"Thoat: "<<endl;
                 for(NODE *k=l.pHead;k!=NULL;k=k->link)
